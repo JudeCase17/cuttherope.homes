e11b5754a00f0a927acea4b57edcf922f271eb9d
aidrum <baukerbeus@gmail.com>
2025-09-20 09:50:18 -0300

    Update topnav.js

--- js/topnav.js
+++ js/topnav.js
@@ -24,7 +24,7 @@
     nav.appendChild(createLink("https://ctrhome.github.io/games", "Games"));
     nav.appendChild(createLink("https://ctrhome.github.io/about", "About"));
     nav.appendChild(createLink("https://ctrhome.github.io/fan-projects", "Fan Projects"));
-    nav.appendChild(createLink("https://ctrhome.github.io/games/cuttherope.ie", "Play Online"));
+    nav.appendChild(createLink("https://ctrhome.github.io/play", "Play Online"));
     nav.appendChild(createLink("https://ctrhome.github.io/modding", "Modding"));
     nav.appendChild(createLink("https://ctrhome.github.io/comics", "Comics"));
 
